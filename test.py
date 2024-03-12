571b3c84341e4be1c036f9b85ee7867125620296
vutl <vutl2004@gmail.com>
2024-03-12 12:08:12 +0700

    first commit

--- test.py
+++ test.py
@@ -13,6 +13,24 @@ wallet_address = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"
 seen_signatures = set()
 solana_client = Client("https://mainnet.helius-rpc.com/?api-key=6dcb92e3-5222-4d11-9dc4-dbee6df8f373")
 
+def get_decimals(transaction, pubkey):
+	try:
+		logs = transaction.transaction.meta.pre_token_balances
+	except AttributeError:
+		logs = [""]
+	for log in logs:
+		if log.mint == pubkey:
+			return log.ui_token_amount.decimals
+
+	try:
+		logs = transaction.transaction.meta.post_token_balances
+	except AttributeError:
+		logs = [""]
+	for log in logs:
+		if log.mint == pubkey:
+			return log.ui_token_amount.decimals
+	return None
+
 
 def getTokens(str_signature):
     signature = Signature.from_string(str_signature)
@@ -27,13 +45,23 @@ def getTokens(str_signature):
             index0 = instructions.accounts[8]
             index1 = instructions.accounts[9]
             index2 = instructions.accounts[7]
+            index3 = get_decimals(transaction, index0)
+            index4 = get_decimals(transaction, index1)
+            index5 = get_decimals(transaction, index0)
+            index6 = 4
+            index7 = Pubkey.from_string('675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8')
             index8 = instructions.accounts[5]
             index9 = instructions.accounts[6]
             index10 = instructions.accounts[12]
             index11 = instructions.accounts[10]
             index12 = instructions.accounts[11]
-            data = {'Index': ['Id', 'baseMint', 'quoteMint', 'IpMint', 'authority', 'openOrders', 'targetOrders', 'baseVault', 'quoteVault'],
-                    'Info': [index, index0, index1, index2, index8, index9, index10, index11, index12]}
+            index13 = Pubkey.from_string("11111111111111111111111111111111")
+            index14 = Pubkey.from_string("11111111111111111111111111111111")
+            index15 = 4
+            index16 = Pubkey.from_string("srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX")
+            index17 = instructions.accounts[16]
+            data = {'Index': ['Id', 'baseMint', 'quoteMint', 'IpMint', 'baseDecimals', 'quoteDecimals', 'IpDecimals', 'version', 'program_id', 'authority', 'openOrders', 'targetOrders', 'baseVault', 'quoteVault', 'withdrawQueue', 'IpVault', 'marketVersion', 'marketProgramId', 'marketId'],
+                    'Info': [index, index0, index1, index2, index3, index4, index5, index6, index7, index8, index9, index10, index11, index12, index13, index14, index15, index16, index17]}
 
             df = pd.DataFrame(data)
             table = tabulate(df, headers='keys', tablefmt='fancy_grid')
