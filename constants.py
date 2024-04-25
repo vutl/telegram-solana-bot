52b4d09db239c28db519ae6e5a46ea9769ec6dcc
vutl <vutl2004@gmail.com>
2024-04-25 14:17:44 +0700

    25/4/2024

--- constants.py
+++ constants.py
@@ -32,8 +32,8 @@ LAMPORTS_PER_SOL = 1000000000
 #for pool_info.py
 API_RAYDIUM_LIQUIDITY_POOL = "https://api.raydium.io/v2/sdk/liquidity/mainnet.json"
 wallet_address = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"
-solana_client = Client("https://mainnet.helius-rpc.com/?api-key=6dcb92e3-5222-4d11-9dc4-dbee6df8f373")
-uri = "wss://mainnet.helius-rpc.com/?api-key=6dcb92e3-5222-4d11-9dc4-dbee6df8f373"
+solana_client = Client("https://devnet.helius-rpc.com/?api-key=6dcb92e3-5222-4d11-9dc4-dbee6df8f373")
+uri = "wss://devnet.helius-rpc.com/?api-key=6dcb92e3-5222-4d11-9dc4-dbee6df8f373"
 SERUM_MARKET_LAYOUT = cStruct(
 	'None' / Bytes(5),
 	'accountFlags' / Bytes(8),
